f5ec516419960c49ccfcffa487426b1e5a240809
Simon Egersand <s.egersand@gmail.com>
2020-07-11 07:49:48 +0200

    Use dynamic port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const validUrl = require("valid-url");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = require("./db");
 
