043d014e43ab757c8334ccf142926f7268b8d3e4
Simon Egersand <s.egersand@gmail.com>
2020-07-11 08:44:59 +0200

    Use spaces over tabs for Node

--- db.js
+++ db.js
@@ -8,41 +8,41 @@ const toDbKey = id => `${KEY_URLS}::${id}`;
 const fromDbKey = key => key.split("::")[1];
 
 exports.insert = async url => {
-	let id = shortid.generate();
+  let id = shortid.generate();
 
-	// Avoid collision
-	let reply = await client.get(toDbKey(id));
-	while (reply) {
-		id = shortid.generate();
-		reply = await client.get(toDbKey(id));
-	}
+  // Avoid collision
+  let reply = await client.get(toDbKey(id));
+  while (reply) {
+    id = shortid.generate();
+    reply = await client.get(toDbKey(id));
+  }
 
-	const key = toDbKey(id);
+  const key = toDbKey(id);
 
-	await client.set(key, url);
-	// Set URL as a key too for searching
-	await client.set(url, key);
+  await client.set(key, url);
+  // Set URL as a key too for searching
+  await client.set(url, key);
 
-	return id;
+  return id;
 };
 
 exports.getByUrl = async url => {
-	const key = await client.get(url);
-	if (key) {
-		return fromDbKey(key);
-	}
+  const key = await client.get(url);
+  if (key) {
+    return fromDbKey(key);
+  }
 
-	return key;
+  return key;
 };
 
 exports.getById = async id => {
-	return await client.get(toDbKey(id));
+  return await client.get(toDbKey(id));
 };
 
 exports.init = callback => {
-	client = redis.createClient(process.env.REDIS_URL);
-	client.on("error", err => console.error(err));
-	client.on("end", () => console.warn("Redis client connection lost"));
-	client.on("warning", warn => console.warn(warn));
-	client.on("ready", callback);
+  client = redis.createClient(process.env.REDIS_URL);
+  client.on("error", err => console.error(err));
+  client.on("end", () => console.warn("Redis client connection lost"));
+  client.on("warning", warn => console.warn(warn));
+  client.on("ready", callback);
 };
--- index.js
+++ index.js
@@ -9,45 +9,45 @@ const port = process.env.PORT || 3000;
 const db = require("./db");
 
 const initExpress = () => {
-	console.log("[init] - Redis connected -> starting express server");
+  console.log("[init] - Redis connected -> starting express server");
 
-	app.use(cors());
-	app.use(bodyParser.urlencoded({extended: false}));
-	app.use(bodyParser.json());
+  app.use(cors());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
 
-	app.post("/minify", async (req, res) => {
-		const url = req.body.url;
-		console.log("[/minify] - Received URL: " + url);
+  app.post("/minify", async (req, res) => {
+    const url = req.body.url;
+    console.log("[/minify] - Received URL: " + url);
 
-		if (!validUrl.isUri(url)) {
-			res.status(400).send("Invalid URL: " + url);
-		} else {
-			// URL is valid
-			const id = await db.getByUrl(url);
+    if (!validUrl.isUri(url)) {
+      res.status(400).send("Invalid URL: " + url);
+    } else {
+      // URL is valid
+      const id = await db.getByUrl(url);
 
-			if (id) {
-				console.log("[/minify] - URL found in DB");
-				res.end(id);
-			} else {
-				console.log("[/minify] - URL not found in DB - storing it");
-				const id = await db.insert(url);
-				res.end(id);
-			}
-		}
-	});
+      if (id) {
+        console.log("[/minify] - URL found in DB");
+        res.end(id);
+      } else {
+        console.log("[/minify] - URL not found in DB - storing it");
+        const id = await db.insert(url);
+        res.end(id);
+      }
+    }
+  });
 
-	app.get("/:id", async (req, res) => {
-		const id = req.params.id;
-		const url = await db.getById(id);
-		if (!url) {
-			res.status(400).send("No matching URL for: " + id);
-		} else {
-			console.log(`[/:id] - Redirecting from ${id} -> ${url}`);
-			res.redirect(url);
-		}
-	});
+  app.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const url = await db.getById(id);
+    if (!url) {
+      res.status(400).send("No matching URL for: " + id);
+    } else {
+      console.log(`[/:id] - Redirecting from ${id} -> ${url}`);
+      res.redirect(url);
+    }
+  });
 
-	app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+  app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 };
 
 db.init(initExpress);
